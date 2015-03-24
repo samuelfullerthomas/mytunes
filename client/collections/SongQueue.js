8533ebfcac9120a9354362805584aab412ec370c
Student <student@MKS-SF-3.local>
2015-03-23 20:01:45 -0500

    Through SonQueue

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -1,7 +1,25 @@
 // SongQueue.js - Defines a backbone model class for the song queue.
 var SongQueue = Songs.extend({
 
-  initialize: function(){
-  }
 
+  initialize: function(){
+    this.on('add', this.enqueue, this);
+    this.on('ended', this.dequeue, this);
+  },
+
+  playFirst: function(){
+    this.at(0).play();
+  },
+
+  enqueue: function(){
+    if (this.length === 1){
+      this.playFirst()
+    }
+  },
+  dequeue: function(){
+    this.shift();
+    if (this.length !== 0){
+      this.playFirst();
+    }
+  },
 });
--- client/views/SongQueueEntryView.js
+++ client/views/SongQueueEntryView.js
@@ -1,4 +1,21 @@
 // SongQueueEntryView.js - Defines a backbone view class for the song queue entries.
 var SongQueueEntryView = Backbone.View.extend({
-  // your code here!
+
+  tagName: 'tr',
+
+  template: _.template('<td>(<%= artist %>)</td><td><%= title %></td>'),
+
+  events: {
+    'add': function() {
+      
+    },
+    'remove': function() {
+
+    }
+  },
+
+  render: function(){
+    return this.$el.html(this.template(this.model.attributes));
+  }
+
 });
